3f201b229c1fafb2ef1cc09b27f98cad092a1ea9
Melow04 <corteskal04@gmail.com>
2024-08-08 21:02:07 +0800

    feat(services): created services for options

--- src/app/pages/services/page.tsx
+++ src/app/pages/services/page.tsx
@@ -1,6 +1,8 @@
 
+import ServiceCard from "@/components/elements/card/service";
 import Footer from "@/components/elements/footer";
 import Header from "@/components/elements/header";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,26 +10,30 @@ import Link from "next/link";
 
 const Services = () => {
     const services: {
-        title: string; content: string; link: string
+        title: string; content: string; img: string; link: string
     }[] = [
             {
                 title: "Personal Training",
-                content: "Innovate your business with tailor-made software solutions.",
+                content: "Achieve your fitness goals with our customized personal training programs.",
+                img: "/features/services/personaltraining.png",
                 link: "/services/custom-software-development",
             },
             {
              title: "Group Classes",
-             content: "Innovate your business with tailor-made software solutions.",
+             content: "Join our dynamic group classes for a fun and effective workout experience.",
+             img: "/features/services/groupclasses.jpg",
             link: "/services/custom-software-development",
             },
             {
                 title: "Nutrition Counseling",
-                content: "Innovate your business with tailor-made software solutions.",
+                content: "Our experts provide personalized nutrition plans to complement your fitness routine.",
+                img: "/features/services/counselling.jpg",
                 link: "/services/custom-software-development",
             },
             {
                 title: "Wellness Programs",
-                content: "Innovate your business with tailor-made software solutions.",
+                content: "Enhance your overall well-being with our holistic wellness programs.",
+                img: "/features/services/wellness.png",
                 link: "/services/custom-software-development",
             }
         ];
@@ -35,29 +41,27 @@ const Services = () => {
 
 return (
     <>
+      <div className="w-full bg-violet-100">
       <Header />
-      <div className="w-full h-screen">
+      <div className="mx-auto w-full max-w-7xl px-5 py-16 grid-cols-2 ">
+      <section>
       <section className="container">
-        <h2 className="py-5 text-5xl font-semibold">Services We Provide</h2>
-        <div className="cardholder grid grid-cols-2 grid-rows-2 gap-4 py-6">
+        <h2 className={cn("py-5 text-5xl font-semibold")}>Services We Provide</h2>
+        <div className={cn("cardholder grid grid-cols-2 grid-rows-2 gap-4 py-6")}>
           {
             services.map((content, index) => (
-              <div key={index} className="border flex p-5 gap-4 rounded-md">
-                <div className="w-full h-100 relative border-2 rounded-md">
-                  <Image src={`/feature/services/${content.title}.jpg`} fill alt={content.title} className="absolute object-cover" />
-                </div>
-                <div className="flex flex-col gap-4">
-                  <h4 className="text-2xl font-bold ">{content.title}</h4>
-                  <p className="line-clamp-3">{content.content}</p>
-                  <Link href={content.link} className="self-end">Check now</Link>
-                </div>
+              <div key={index}>
+                <ServiceCard
+                  content={content}
+                />
               </div>
             ))
           }
         </div>
       </section>
-    </div>
-        <Footer />
+      </section></div>
+       </div> <Footer />
+  
     </>
 );
 }
